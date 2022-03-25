c69e06ade242ec16aa648bd8c61aa75a7398cf41
Sarfraj555 <87345420+Sarfraj555@users.noreply.github.com>
2022-03-25 13:10:38 +0530

    Update afterinstall.sh

--- afterinstall.sh
+++ afterinstall.sh
@@ -2,5 +2,7 @@
 
 cd /home/ubuntu
 npm -f install
-$(/home/ubuntu/.local/bin/aws ecr get-login --no-include-email --region us-east-1)
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
